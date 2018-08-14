cacbe81292a5d9862fdc4b163a1c6ed3889b02de
Piotr Piech <piotrpiech@wp.pl>
2018-08-14 17:29:41 +0200

    dodane widoki z nagłówkiem + drobne poprawki w nazwach

---
+++ src/main/java/pl/coderslab/Employees.java
@@ -0,0 +1,21 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Employees", urlPatterns = "/employees")
+public class Employees extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext()
+                .getRequestDispatcher("//employees.jsp")
+                .forward(request,response);
+    }
+}
--- src/main/webapp/employees.jsp
+++ src/main/webapp/employees.jsp
@@ -2,15 +2,14 @@
   Created by IntelliJ IDEA.
   User: migiel
   Date: 13.08.18
-  Time: 10:31
+  Time: 10:33
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
+
+<%@ include file="WEB-INF/includes/header.jspf" %>
+
+<h2>Pracownicy</h2>
 
 </body>
 </html>
