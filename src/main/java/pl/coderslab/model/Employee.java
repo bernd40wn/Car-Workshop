bccb468f078dd164ebda78d450b9963849cd5f93
FilipMikulski <mikulskifilip@gmail.com>
2018-08-15 10:44:48 +0200

    customer i employee

--- src/main/java/pl/coderslab/model/Employee.java
+++ src/main/java/pl/coderslab/model/Employee.java
@@ -1,4 +1,79 @@
 package pl.coderslab.model;
 
 public class Employee {
+    private String name;
+    private String surname;
+    private String adress;
+    private int phonenumber;
+    private String note;
+    private float workhours;
+    private int id = 0;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String adress, int phonenumber, String note, float workhours) {
+        this.name = name;
+        this.surname = surname;
+        this.adress = adress;
+        this.phonenumber = phonenumber;
+        this.note = note;
+        this.workhours = workhours;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public void setWorkhours(float workhours) {
+        this.workhours = workhours;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public float getWorkhours() {
+        return workhours;
+    }
 }
