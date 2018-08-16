2bd4d63f30e2cda72114233d0809bdafcf37f34e
wrobljac <wrobljac@gmail.com>
2018-08-16 18:09:18 +0200

    Dodawanie, edycja i usuwanie klientow

--- src/main/java/pl/coderslab/CustomerEdit.java
+++ src/main/java/pl/coderslab/CustomerEdit.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Date;
-import java.util.ArrayList;
 
 @WebServlet(name = "CustomerEdit", urlPatterns = "/customer/edit")
 public class CustomerEdit extends HttpServlet {
